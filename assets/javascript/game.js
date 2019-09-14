01d7e2d74d44be16398dc643d38d1344507cda1a
Miyoung Chung <mkchung.8@gmail.com>
2019-09-13 23:02:46 -0700

    simplified HTML structure and changed id's. Also modified game variables in Javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,8 @@ $(document).ready(function(){
 
     
     //Character Objects 
-  
        
-   var johnnyCage = {id: "johnnyc", name:"Johnny Cage", attack: 3, counterattack: 10, pic: '<img class="character-image" src="./assets/images/johnnycage.jpg">'};
+   var johnnyc = {id: "johnnyc", name:"Johnny Cage", attack: 3, counterattack: 10, pic: '<img class="character-image" src="./assets/images/johnnyc.jpg">'};
    var mileena = {id: "mileena", name: "Mileena", attack: 2, counterattack: 10, pic: '<img class="character-image" src="./assets/images/mileena.jpg">'};
    var raiden = {id: "raiden", name: "Raiden", attack: 5, counterattack: 20, pic: '<img class="character-image" src="./assets/images/raiden.jpg">'};
    var scorpion = {id: "scorpion", name:"Scorpion", attack: 4, counterattack: 5, pic: '<img class="character-image" src="./assets/images/scorpion.jpg">'};
@@ -14,9 +13,12 @@ $(document).ready(function(){
 
     //Setting game variables
     var turnLock = false; 
+    var characterLock = false;
+    var userPlayer;
+    var opponentPlayer;
     var playerHealth = 100; 
     var oppponentHealth = 100; 
-    var playerArray = [johnnyCage, mileena, raiden, scorpion, sonya];
+    var playerArray = [johnnyc, mileena, raiden, scorpion, sonya];
     var playerList; 
     var playerHealth; 
     var opponentHealth;
@@ -24,30 +26,46 @@ $(document).ready(function(){
     var opponentAttack;
 
 
+
     //Initialize game 
     initializeGame();
     //playGame();
    // resetGame();
 
+// Function that intializes the character display list
    function startCharacters() {
        for (var i=0; i < playerArray.length; i++) {
-           var character = $("<div>");
+           var character = $('<div>');
            character.addClass("col-md-2 character-div");
-           character.attr("character-id", playerArray[i].id);
+           character.attr("player-id", playerArray[i].id);
+           character.attr("data_attack", playerArray[i].attack);
            character.html("<p>" + playerArray[i].name + "</p>" + playerArray[i].pic);
            $("#character-list").append(character);
-       }
-        
-        }
-   
+         };
+       };
 
+       function playerPick() {
+        $(".character-div").on("click", function(){
+            let playerId = $(this).attr("player-id");
+            $("#player-div").html()
+            console.log()
+            $("#player-div").html("<img class='character-image' src='./assets/images/" + playerId + ".jpg'>");
+            
+        });
+       };
+       
+       
+       
+  
+
+//Game start 
     function initializeGame () {
         //Starting game message 
         $("#game-message").html("<h1>Welcome to Mortal Kombat. Choose your fighter. </h1>");
 
         
         startCharacters();
-    //    playerPick();
+        playerPick();
      //   opponentPick();
         };
     
