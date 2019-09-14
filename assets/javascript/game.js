3c6512434bfc6e203076a9f0befb7273cfea7f4f
Miyoung Chung <mkchung.8@gmail.com>
2019-09-14 04:47:27 -0700

    updated player select function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,51 +17,70 @@ $(document).ready(function(){
     var turnLock = true; 
     var characterLock = false;
     var player;
+    var playerId;
     var opponent;
     var attackMultiplier; 
     var playerHealth = 100; 
     var oppponentHealth = 100; 
-    var opponentList = []; 
     var playerHealth; 
     var opponentHealth;
+    var opponentList = [];
 
     initializeGame();
-    playerPick();
-
     
+    opponentPick();
     function initializeGame () {
 
         //Starting game message 
         $("#game-message").html("<h1>Welcome to Mortal Kombat. Choose your fighter. </h1>");
         
         for (var key in characterList) {
-            startCharacters(characterList[key],"#character-list");
+            startCharacters(characterList[key],"#character-list"); 
         };
+
+
      };
+    
      
 
-    function playerPick (charObj){
-        $(".character-div").on("click", function(){
-            var playerId = $(this).attr("character-id");
-            $(this).remove();
-            var playerDiv = $('<div class="player-selected" character-id="' + playerId + '">');
-
-        });
-
-    };
-
-    function opponentPick (charObj) {
-
-    };
-    
-        //playerPick();
-     //   opponentPick();
      function startCharacters (character, characterArea) {
-        var characterDiv = $('<div class="character-div" character-id="' + character.id + '">');
+        var characterDiv = $('<div class="character-div" character-id="' + character.id + '">')
         var characterName = $('<div class="character-name">').text(character.name);
         var characterImage = $('<img class="character-image">').attr("src",character.pic);
         characterDiv.append(characterName).append(characterImage);
         $(characterArea).append(characterDiv);
+        opponentList.push(character.id);
     };    
 
+
+    
+   
+        
+        $("#character-list").on("click", ".character-div", function(){
+            var playerId = $(this).attr("character-id");
+            $(this).remove();
+            $("#player-div").append($(this));
+            characterLock = true;
+        }); 
+        
+        if (characterLock === true) {
+            $("#character-list").attr("id", "opponent-list");
+            alert("characterlock")
+        } else {
+
+        };
+    
+
+    
+
+    function opponentPick(){
+        
+
+    };
+   
+    
+
+    
+
+    
 });
