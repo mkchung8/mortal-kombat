a63899979118301f8174d3929411c5203f09633f
Miyoung Chung <mkchung.8@gmail.com>
2019-09-14 00:18:57 -0700

    modified properties of objects and updated character list array upload with jQuery also removing unecessary javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,58 @@
 
 $(document).ready(function(){
+ 
+    //Character Object 
 
-    
-    //Character Objects 
-       
-   var johnnyc = {id: "johnnyc", name:"Johnny Cage", attack: 3, counterattack: 10, pic: '<img class="character-image" src="./assets/images/johnnyc.jpg">'};
-   var mileena = {id: "mileena", name: "Mileena", attack: 2, counterattack: 10, pic: '<img class="character-image" src="./assets/images/mileena.jpg">'};
-   var raiden = {id: "raiden", name: "Raiden", attack: 5, counterattack: 20, pic: '<img class="character-image" src="./assets/images/raiden.jpg">'};
-   var scorpion = {id: "scorpion", name:"Scorpion", attack: 4, counterattack: 5, pic: '<img class="character-image" src="./assets/images/scorpion.jpg">'};
-   var sonya = {id: "sonya", name: "Sonya", attack: 4, counterattack: 15, pic: '<img class="character-image" src="./assets/images/sonya.jpg">'};
+    var characterList = {
 
+    "johnnyc" : {id: "johnnyc", name:"Johnny Cage", attack: 3, counterattack: 10, pic: "assets/images/johnnyc.jpg"},
+    "mileena" : {id: "mileena", name: "Mileena", attack: 2, counterattack: 10, pic: "assets/images/mileena.jpg"},
+    "raiden"  :{id: "raiden", name: "Raiden", attack: 5, counterattack: 20, pic: "assets/images/raiden.jpg"},
+    "scorpion" : {id: "scorpion", name:"Scorpion", attack: 4, counterattack: 5, pic: "assets/images/scorpion.jpg"},
+    "sonya" : {id: "sonya", name: "Sonya", attack: 4, counterattack: 15, pic: "assets/images/sonya.jpg"},
+
+    };
 
     //Setting game variables
     var turnLock = false; 
     var characterLock = false;
-    var userPlayer;
-    var opponentPlayer;
+    var player;
+    var opponent;
+    var attackMultiplier; 
     var playerHealth = 100; 
     var oppponentHealth = 100; 
-    var playerArray = [johnnyc, mileena, raiden, scorpion, sonya];
-    var playerList; 
+    var fightList = []; 
     var playerHealth; 
     var opponentHealth;
-    var playerAttack; 
-    var opponentAttack;
+
+    var startCharacters = function (character, characterArea) {
+        var characterDiv = $('<div class="character-div" character-id="' + character.id + '">')
+        var characterName = $('<div class="character-name>').text(character.name);
+        var characterImage = $('<img class="character-image">').attr("src",character.pic);
+        characterDiv.append(characterName).append(characterImage);
+        $(characterArea).append(characterDiv);
+    };
 
 
 
-    //Initialize game 
-    initializeGame();
-    //playGame();
-   // resetGame();
-
-// Function that intializes the character display list
-   function startCharacters() {
-       for (var i=0; i < playerArray.length; i++) {
-           var character = $('<div>');
-           character.addClass("col-md-2 character-div");
-           character.attr("player-id", playerArray[i].id);
-           character.attr("data_attack", playerArray[i].attack);
-           character.html("<p>" + playerArray[i].name + "</p>" + playerArray[i].pic);
-           $("#character-list").append(character);
-         };
-       };
-
-       function playerPick() {
-        $(".character-div").on("click", function(){
-            let playerId = $(this).attr("player-id");
-            $("#player-div").html()
-            console.log()
-            $("#player-div").html("<img class='character-image' src='./assets/images/" + playerId + ".jpg'>");
-            
-        });
-       };
-       
-       
-       
-  
-
-//Game start 
-    function initializeGame () {
-        //Starting game message 
-        $("#game-message").html("<h1>Welcome to Mortal Kombat. Choose your fighter. </h1>");
 
         
-        startCharacters();
-        playerPick();
-     //   opponentPick();
+  
+
+//Function to initialize the game by loading characters into the character div
+    function initializeGame () {
+
+        //Starting game message 
+        $("#game-message").html("<h1>Welcome to Mortal Kombat. Choose your fighter. </h1>");
+        
+        for (var key in characterList) {
+            startCharacters(characterList[key],"#character-list");
         };
-    
-         
-    /*function startCharacters() {
-        for (i=0; i < playerArray.length; i++) {
-            console.log(playerArray[i].name);
-            $("<div>").each()
-            var character = 
-        }
-    }
-    */
-    
+     };
+
+     initializeGame();
+        //playerPick();
+     //   opponentPick();
+        
 
 });
