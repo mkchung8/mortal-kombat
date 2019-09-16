fd138957e0a77c04d59cf3f32b07100a0f9ba80d
Miyoung Chung <mkchung.8@gmail.com>
2019-09-15 17:38:19 -0700

    added functions to update opponent list array and pseucode for rest of game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
 
     var characterList = {
 
-    "johnnyc" : {id: "johnnyc", name:"Johnny Cage", attack: 3, counterattack: 10, pic: "assets/images/johnnyc.jpg"},
-    "mileena" : {id: "mileena", name: "Mileena", attack: 2, counterattack: 10, pic: "assets/images/mileena.jpg"},
-    "raiden"  :{id: "raiden", name: "Raiden", attack: 5, counterattack: 20, pic: "assets/images/raiden.jpg"},
-    "scorpion" : {id: "scorpion", name:"Scorpion", attack: 4, counterattack: 5, pic: "assets/images/scorpion.jpg"},
-    "sonya" : {id: "sonya", name: "Sonya", attack: 4, counterattack: 15, pic: "assets/images/sonya.jpg"},
+    "johnnyc" : {id: "johnnyc", name:"Johnny Cage", attack: 3, pic: "assets/images/johnnyc.jpg"},
+    "mileena" : {id: "mileena", name: "Mileena", attack: 2, pic: "assets/images/mileena.jpg"},
+    "raiden"  :{id: "raiden", name: "Raiden", attack: 5, pic: "assets/images/raiden.jpg"},
+    "scorpion" : {id: "scorpion", name:"Scorpion", attack: 4, pic: "assets/images/scorpion.jpg"},
+    "sonya" : {id: "sonya", name: "Sonya", attack: 4, pic: "assets/images/sonya.jpg"},
 
     };
 
@@ -17,13 +17,13 @@ $(document).ready(function(){
     var turnLock = true; 
     var characterLock = false;
     var player;
+    var playerAttack;
+    var counterAttack;
     var playerLock = [];
     var opponent;
     var attackMultiplier; 
-    var playerHealth = 100; 
-    var oppponentHealth = 100; 
     var playerHealth; 
-    var opponentHealth;
+    var opponentHealth; 
     var opponentList = [];
 
     initializeGame();
@@ -38,12 +38,15 @@ $(document).ready(function(){
             startCharacters(characterList[key],"#character-list"); 
         };
 
+        //empty playerLock
+
+        playerHealth = 100; 
 
      };
     
 
      function startCharacters (character, characterArea) {
-        var characterDiv = $('<div class="character-div" character-id="' + character.id + '">')
+        var characterDiv = $('<div class="character-div" card-id="' + character.id + '">');
         var characterName = $('<div class="character-name">').text(character.name);
         var characterImage = $('<img class="character-image">').attr("src",character.pic);
         characterDiv.append(characterName).append(characterImage);
@@ -55,19 +58,28 @@ $(document).ready(function(){
    $("#character-list").on("click", ".character-div", function (){
         var idPick = $(this).attr("character-id");
         playerLock.push(idPick);
+        player = playerLock[0];
+        
+        
 
         if (playerLock.length === 0) {
             initializeGame();
+
+            
            } else if (playerLock.length > 0) {
-                
+                player = playerLock[0];
                $(this).remove();
                var player = $("#player-div")
                player.append($(this)); 
+
                playerSet();
                opponentUpdate();
                opponentSet();
                gamePlay();
            };
+
+
+
         
    });
    
@@ -77,25 +89,48 @@ $(document).ready(function(){
    
    function playerSet(character, playerArea) {
 
-        var playerSet = $('<div class="player-card character-id="' + playerLock[0] + '">');
-        var playerId = playerLock[0];
-        var playerName = $("")
-        
-        
+    $("#player-div").append('<h3 class="health-display">Your Health: ' + playerHealth + '</h3>')
+       
         
    };
 
+
    function opponentUpdate (opponent) {
-       opponentList = opponentList.filter(e => e !== playerLock[0]);
-       
-       
        
    };
 
    function opponentSet (){
+        opponentHealth = 100;
+        counterAttack = 5; 
+       $("#opponent-div").append('<h3 class="health-display">Opponent Health: ' + opponentHealth + '</h3>');
+        opponentList = opponentList.filter(e => e !== playerLock[0]);
+        var indexRandomize = Math.floor(Math.random() * opponentList.length);
+        var opponentSelect = opponentList[indexRandomize];
+        console.log(opponentSelect)
+    
+        $("#opponent-div").append("<h1>enemy goes here</h1>")
 
-   }
-   function gamePlay (){};
+        $(".character-div").attr(opponentSelect)
+
+    
+    
+   };
+
+   function gamePlay (){
+
+    //initialize attack multiplier
+    // When user clicks attack 
+    // disable attack button until opponent turn is over 
+    //subtract attack points from opponent health
+    //opponent attacks, subtracting attack points from player health
+    //enable attack button 
+    //update attack multiplier
+    //if opponent health < 0, select new opponent 
+    //if user health < 0, Game over 
+    //when opponentList.length = 0, User Wins 
+
+
+   };
     
    
 
